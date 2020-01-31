8b76cf482c5bff0e83a2214afe34bf3c304a2b2b
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-01-31 16:20:04 +0700

    Added the layout component for the main page

---
+++ src/components/Mainpage/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-four-fifths-tablet is-three-fifths-desktop">
+      <div class="card">
+        <div class="card-content">
+          <div class="content marginBottom10px">
+            <slot name="option"></slot>
+            <slot name="body"></slot>
+            <slot name="pagination"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.marginBottom10px {
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/components/Mainpage/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-half is-size-3-touch is-size-2-desktop has-text-centered has-text-weight-bold">
+      <div class="marginTop10px">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.marginTop10px {
+  margin-top: 10px;
+}
+</style>
