3e237346631239d15448ca62fb80cc8d5b3d8e99
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-02-03 20:07:24 +0700

    Updated the news service

---
+++ src/services/news.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { API_URL } from "@/config/environments.js";
+
+const headerOption = { headers: { 'Content-Type': 'application/json' } };
+
+export default {
+    getAllNews: async () => {
+      return await axios.get(`${API_URL}/news`, headerOption);
+    },
+    getNewsById: async (payload) => {
+      return await axios.get(`${API_URL}/news/${payload.id}`, headerOption);
+    },
+    createNews: async (payload) => {
+      return await axios.post(`${API_URL}/news`, payload);
+    },
+    updateNewsById: async (payload) => {
+      return await axios.put(`${API_URL}/news/${payload.id}`, payload.data ,headerOption);
+    } , 
+    deleteNewsById: async (payload) => {
+      return await axios.delete(`${API_URL}/news/${payload.id}`, headerOption);
+    }
+}
